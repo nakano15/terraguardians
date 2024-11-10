9f43e14fab043aa4f3a1a46f5d18adccc0f2eda2
Nakano15 <douglasgoldbach@gmail.com>
2024-11-10 00:43:05 -0300

    Changed the head drawing layer to behave differently when generating world.

--- CompanionHeadsMapLayer.cs
+++ CompanionHeadsMapLayer.cs
@@ -1,4 +1,5 @@
 using Terraria;
+using System.Linq;
 using Terraria.DataStructures;
 using Terraria.GameContent;
 using Terraria.Graphics.Renderers;
@@ -17,6 +18,11 @@ namespace terraguardians
 
         public override void Draw(ref MapOverlayDrawContext context, ref string text)
         {
+			if (Main.gameMenu)
+			{
+				DrawMenuMode(ref context, ref text);
+				return;
+			}
             foreach(Companion c in MainMod.ActiveCompanions.Values)
             {
                 if (!WorldMod.HasMetCompanion(c.Data) || c.IsHostileTo(MainMod.GetLocalPlayer) || c.invis || !c.GetGoverningBehavior().IsVisible)
@@ -46,6 +52,39 @@ namespace terraguardians
                 }*/
             }
         }
+		
+		void DrawMenuMode(ref MapOverlayDrawContext context, ref string text)
+		{
+			Companion[] ToDraw = MainMod.ActiveCompanions.Values.ToArray();
+            foreach(Companion c in ToDraw)
+            {
+                if (!WorldMod.HasMetCompanion(c.Data) || c.IsHostileTo(MainMod.GetLocalPlayer) || c.invis || !c.GetGoverningBehavior().IsVisible)
+                    continue;
+                if (c is TerraGuardian)
+                {
+                    Texture2D head = c.Base.GetSpriteContainer.HeadTexture;
+                    if (head != null && context.Draw(head, (c.Bottom - Vector2.UnitY * c.SpriteHeight) * (1f / 16), Color.White, new SpriteFrame(1, 1, 0, 0), 1f, 1f, Terraria.UI.Alignment.Center).IsMouseOver)
+                    {
+                        text = c.GetName;
+                    }
+                }
+                /*else
+                {
+                    playerHead = new PlayerHeadDrawRenderTargetContent();
+                    playerHead.UsePlayer(c);
+                    playerHead.UseColor(default(Color));
+                    playerHead.Request();
+                    if (playerHead.IsReady)
+                    {
+                        RenderTarget2D target = playerHead.GetTarget();
+                        if (context.Draw(target, c.Top * (1f / 16), Color.White, new SpriteFrame(1, 1, 0, 0), 1f, 2f, Terraria.UI.Alignment.Center).IsMouseOver)
+                        {
+                            text = c.name;
+                        }
+                    }
+                }*/
+            }
+		}
 
         internal static void OnUnload()
         {
